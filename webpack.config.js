1433508951ec429dbd13491b758eaad2b18f3042
alex <nowaalex@gmail.com>
2020-02-10 15:50:52 +0200

    added example

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
+const path = require( "path" );
 const HtmlWebpackPlugin = require( "html-webpack-plugin" );
 
 module.exports = {
     entry: "./example/index.js",
+    output: {
+        path: path.resolve(__dirname, "exampleAssets" )
+    },
 	module: {
 		rules: [
 			{
