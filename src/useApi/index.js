13eaf941e5a25cec6cce85b594faa8d81feaaa56
alex <nowaalex@gmail.com>
2020-02-17 06:16:47 +0200

    +

--- src/useApi/index.js
+++ src/useApi/index.js
@@ -3,24 +3,24 @@ import Context from "../Context";
 
 const emptyArr = [];
 
-const reducer = () => performance.now();
+const reducer = x => x + 1;
 
 /*
 	TODO:
 		why useEffect does not properly update colgroup of tbody, when rowcount changes from 0 to positive value?
 */
 
-const useApiPluginRaw = (API, subscribeEvents) => {
+const useApiPluginRaw = ( ApiEvents, subscribeEvents) => {
 
-    const [stamp, up] = useReducer( reducer, 0 );
+    const [, up] = useReducer( reducer, 0 );
     
 	useLayoutEffect(() => {
 		for (let e of subscribeEvents) {
-			API.Events.on(e, up);
+			ApiEvents.on(e, up);
 		}
 		return () => {
 			for (let e of subscribeEvents) {
-				API.Events.off(e, up);
+				ApiEvents.off(e, up);
 			}
 		};
 	}, emptyArr );
@@ -30,6 +30,6 @@ export const useApiContext = () => useContext( Context );
 
 export const useApiPlugin = subscribeEvents => {
 	const API = useApiContext();
-    useApiPluginRaw( API, subscribeEvents );
+    useApiPluginRaw( API.Events, subscribeEvents );
     return API;
 };
