c35abf947835715089ef7f12c2acc8d7e6d89679
alex <nowaalex@gmail.com>
2020-02-18 05:51:32 +0200

    added source-map to example

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require( "html-webpack-plugin" );
 
 module.exports = {
     entry: "./example/index.js",
+    devtool: "source-map",
     output: {
         path: path.resolve(__dirname, "exampleAssets" )
     },
