153a1bde868de60fff93c5542bdc373b481db747
alex <nowaalex@gmail.com>
2020-07-01 17:08:51 +0300

    +

--- website/19.2b5e44113566c1d2d590.js
+++
@@ -1,3 +0,0 @@
-/*! For license information please see 19.2b5e44113566c1d2d590.js.LICENSE.txt */
-(window.Z=window.Z||[]).push([[19],{212:function(t,e,n){"use strict";n.r(e);n(0);var a=n(64),r=n(6),o=function(t){return"row "+t};e.default=function(t){var e=t.className;return Object(r.c)(a.a,{fixedSize:!0,className:e,getRowData:o,rowCount:5e5})}}}]);
-//# sourceMappingURL=sm.19.3e32ebd002ee4e2692165cf68911e098.map
--- website/21.4073b9027539bcd76e27.js
+++
@@ -1,3 +0,0 @@
-/*! For license information please see 21.4073b9027539bcd76e27.js.LICENSE.txt */
-(window.Z=window.Z||[]).push([[21],{1323:function(a,c,e){"use strict";e.r(c);e(0);var t=e(63),l=e(6),n=[{dataKey:"a",label:"a"},{dataKey:"b",label:"b"},{dataKey:"c",label:"c"}],o=function(a){return{a:a,b:"cell_b_row: "+a,c:"cell_c_row: "+a}};c.default=function(a){var c=a.className;return Object(l.c)(t.a,{className:c,getRowData:o,nonSticky:!0,rowCount:500,columns:n})}}}]);
-//# sourceMappingURL=sm.21.2e7aeca6789bfbc1abcbc9eaa1c25b72.map
