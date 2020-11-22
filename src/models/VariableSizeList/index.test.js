07f01f02fa53643fecc505525e1e746447320f63
alex <nowaalex@gmail.com>
2020-11-22 02:35:50 +0200

    +

--- src/models/VariableSizeList/index.test.js
+++ src/models/VariableSizeList/index.test.js
@@ -1,25 +1,56 @@
 import VariableSizeList from "./index";
+import r from "lodash/random";
+
+const ROWS_QUANTITY = 100;
+const UPDATES_QUANTITY = 15;
+
+const ROW_HEIGHTS = new Uint32Array( ROWS_QUANTITY );
+
+const getOffsetUnoptimized = n => {
+    let offset = 0;
+    for( let j = 0; j < n; j++ ){
+        offset += ROW_HEIGHTS[ j ];
+    }
+    return offset;
+}
+
+const getIndexUnoptimized = offset => {
+    let index = -1;
+
+    do {
+        offset -= ROW_HEIGHTS[ ++index ];
+    }
+    while( offset >= 0 );
+
+    return index;
+}
 
 describe( "VariableSizeList works ok", () => {
 
     test( "it sums ok", () => {
 
         const VSList = new VariableSizeList();
-        VSList.setViewParams( 0, 2, 7, null );
+        VSList.setViewParams( 0, 2, ROWS_QUANTITY, null );
 
-        VSList.updateRowHeight( 0, 10 );
-        VSList.updateRowHeight( 1, 20 );
-        VSList.updateRowHeight( 2, 30 );
-        VSList.updateRowHeight( 3, 40 );
-        VSList.updateRowHeight( 4, 50 );
 
-        expect( VSList.getOffset( 4 ) ).toBe( 100 );
+        for( let i = 0, sum; i < UPDATES_QUANTITY; i++ ){
 
-        VSList.updateRowHeight( 1, 36 );
+            sum = 0;
 
-        expect( VSList.getOffset( 2 ) ).toBe( 66 );
+            for( let j = 0, diff; j < ROWS_QUANTITY; j++ ){
+                diff = r( 5 - i, 20 );
+                sum += diff;
+                VSList.updateRowHeight( j, diff );
+                ROW_HEIGHTS[ j ] += diff;
+            }
 
-        expect( VSList.getIndex( 67 ) ).toBe( 2 );
-        expect( VSList.getIndex( 95 ) ).toBe( 2 );
+            for( let j = 0; j < ROWS_QUANTITY; j++ ){
+                expect( VSList.getOffset( j ) ).toBe( getOffsetUnoptimized( j ) );
+            }
+    
+            for( let j = 0; j < sum; j++ ){
+                expect( VSList.getIndex( j ) ).toBe( getIndexUnoptimized( j ) );
+            }
+        }
     });
 })
