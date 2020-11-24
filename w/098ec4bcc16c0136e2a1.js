2a2f64b9501775158a0ef05f0f770096d23b447f
alex <nowaalex@gmail.com>
2020-11-24 04:41:46 +0200

    upgrade & clean

--- w/098ec4bcc16c0136e2a1.js
+++
@@ -1,3 +0,0 @@
-/*! For license information please see 098ec4bcc16c0136e2a1.js.LICENSE.txt */
-(self.webpackChunkaf_virtual_scroll=self.webpackChunkaf_virtual_scroll||[]).push([[483],{19756:(e,l,t)=>{"use strict";function c(e,l){if(null==e)return{};var t,c,a={},r=Object.keys(e);for(c=0;c<r.length;c++)t=r[c],l.indexOf(t)>=0||(a[t]=e[t]);return a}t.d(l,{Z:()=>c})},40610:(e,l,t)=>{"use strict";t.r(l),t.d(l,{default:()=>r});var c=t(85893),a=t(34550);const r=()=>(0,c.jsx)(a.Z,{fixed:!0,rowsQuantity:1e3,getRowData:e=>({a:"cell_a_"+e,b:"cell_b_"+e,c:"cell_c_"+e}),columns:["a","b","c"]})}}]);
-//# sourceMappingURL=098ec4bcc16c0136e2a1.js.map
--- w/6d8dfb703e10b4a928dd.js
+++
@@ -1,3 +0,0 @@
-/*! For license information please see 6d8dfb703e10b4a928dd.js.LICENSE.txt */
-(self.webpackChunkaf_virtual_scroll=self.webpackChunkaf_virtual_scroll||[]).push([[781],{19756:(e,l,t)=>{"use strict";function a(e,l){if(null==e)return{};var t,a,c={},s=Object.keys(e);for(a=0;a<s.length;a++)t=s[a],l.indexOf(t)>=0||(c[t]=e[t]);return c}t.d(l,{Z:()=>a})},33862:(e,l,t)=>{"use strict";t.r(l),t.d(l,{default:()=>s});var a=t(85893),c=t(34550);const s=()=>(0,a.jsx)(c.Z,{fixed:!0,headless:!0,rowsQuantity:1e3,getRowData:e=>({a:"cell_a_"+e,b:"cell_b_"+e,c:"cell_c_"+e}),columns:["a","b","c"]})}}]);
-//# sourceMappingURL=6d8dfb703e10b4a928dd.js.map
