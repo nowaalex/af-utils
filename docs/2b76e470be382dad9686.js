1c93925a6db6df8ce1a8b6c0a645b264567941f0
alex <nowaalex@gmail.com>
2021-04-03 12:37:30 +0300

    upgraded some deps in yarn.lock to fix github security issues

---
+++ docs/2b76e470be382dad9686.js
@@ -0,0 +1,3 @@
+/*! For license information please see 2b76e470be382dad9686.js.LICENSE.txt */
+(self.webpackChunkaf_virtual_scroll=self.webpackChunkaf_virtual_scroll||[]).push([[799],{40610:(a,l,e)=>{"use strict";e.r(l),e.d(l,{default:()=>s});var c=e(87113),t=e(85893);const s=()=>(0,t.jsx)(c.Z,{fixed:!0,rowsQuantity:1e3,getRowData:a=>({a:`cell_a_${a}`,b:`cell_b_${a}`,c:`cell_c_${a}`}),columns:[{dataKey:"a",label:"a",width:"100em"},{dataKey:"b",label:"b"},{dataKey:"c",label:"c"}]})}}]);
+//# sourceMappingURL=2b76e470be382dad9686.js.map
---
+++ docs/efb4207b6beb2de03d5f.js
@@ -0,0 +1,3 @@
+/*! For license information please see efb4207b6beb2de03d5f.js.LICENSE.txt */
+(self.webpackChunkaf_virtual_scroll=self.webpackChunkaf_virtual_scroll||[]).push([[774],{33862:(a,l,e)=>{"use strict";e.r(l),e.d(l,{default:()=>t});var c=e(87113),s=e(85893);const t=()=>(0,s.jsx)(c.Z,{fixed:!0,headless:!0,rowsQuantity:1e3,getRowData:a=>({a:`cell_a_${a}`,b:`cell_b_${a}`,c:`cell_c_${a}`}),columns:[{dataKey:"a",label:"a"},{dataKey:"b",label:"b"},{dataKey:"c",label:"c"}]})}}]);
+//# sourceMappingURL=efb4207b6beb2de03d5f.js.map
