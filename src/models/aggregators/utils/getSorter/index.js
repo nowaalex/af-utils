7b0e02904f4e229ae26454ccdb5b5041d63f2727
alex <nowaalex@gmail.com>
2021-05-10 03:34:23 +0300

    2.7.2

--- src/models/aggregators/utils/getSorter/index.js
+++ src/models/aggregators/utils/getSorter/index.js
@@ -1,14 +1,19 @@
-const getSorter = ( getRowData, sortDataKey, sortDirection ) => ( a, b ) => {
-    const row1 = getRowData( a );
-    const row2 = getRowData( b );
+const collator = new Intl.Collator( undefined, { numeric: true });
 
-    if( row1 && row2 ){
-        const v1 = row1[ sortDataKey ];
-        const v2 = row2[ sortDataKey ];
-        return v1 > v2 ? sortDirection : v1 < v2 ? -sortDirection : 0;
+const getSorter = ( getRowData, sortDataKey, sortDirection ) => {
+
+    return ( a, b ) => {
+        const row1 = getRowData( a );
+        const row2 = getRowData( b );
+
+        if( row1 && row2 ){
+            const v1 = row1[ sortDataKey ];
+            const v2 = row2[ sortDataKey ];
+            return collator.compare( v1, v2 ) * sortDirection;
+        }
+
+        return row1 ? sortDirection : row2 ? -sortDirection : 0;
     }
-
-    return row1 ? sortDirection : row2 ? -sortDirection : 0;
 }
 
 export default getSorter;
