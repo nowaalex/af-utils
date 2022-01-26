d8c65a07d78fc89e0b8d6ff7ec8904f40c37302d
alex <nowaalex@gmail.com>
2022-01-26 17:23:11 +0200

    moved subscription component

--- packages/af-virtual-scroll/src/components/Subscription/index.js
+++ packages/af-virtual-scroll/src/components/Subscription/index.js
@@ -4,7 +4,6 @@ import useSubscription from "hooks/useSubscription";
 const inc = x => x + 1;
 
 const Subscription = ({ model, children }) => {
-
     const [ forceUpdateRenderCounter, forceUpdate ] = useReducer( inc, 1 );
 
     useSubscription( model, forceUpdate );
--- packages/af-virtual-scroll/src/index.js
+++ packages/af-virtual-scroll/src/index.js
@@ -4,7 +4,7 @@ export { default as mapVisibleRange } from "./utils/mapVisibleRange";
 export { default as useVirtual } from "./hooks/useVirtual";
 export { default as useSubscription } from "./hooks/useSubscription";
 
-export { default as Subscription } from "./components/common/Subscription";
+export { default as Subscription } from "./components/Subscription";
 export { default as VerticalList } from "./components/VerticalList";
 export { default as Table } from "./components/Table";
 
