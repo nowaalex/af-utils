428b281a3314a87565af4a4f201a4cd400be5912
alex <nowaalex@gmail.com>
2022-08-11 03:07:00 +0200

    improved syntax highlight

--- packages/website/next.config.mjs
+++ packages/website/next.config.mjs
@@ -1,23 +1,26 @@
 import remarkGfm from "remark-gfm";
 import { refractor } from "refractor";
 import rehypeRewrite from "rehype-rewrite";
-import rehypePrism from "@mapbox/rehype-prism";
 import remarkToc from "remark-toc";
 import nextMdx from "@next/mdx";
 import rehypeSlug from "rehype-slug";
 import nextBundleAnalyzer from "@next/bundle-analyzer";
 
+import typescript from "refractor/lang/typescript.js";
+import javascript from "refractor/lang/javascript.js";
 import jsx from "refractor/lang/jsx.js";
 import css from "refractor/lang/css.js";
+import scss from "refractor/lang/scss.js";
+import sass from "refractor/lang/sass.js";
 import bash from "refractor/lang/bash.js";
-import jsExtras from "refractor/lang/js-extras.js";
-import cssExtras from "refractor/lang/css-extras.js";
 
+refractor.register(typescript);
+refractor.register(javascript);
 refractor.register(jsx);
 refractor.register(css);
+refractor.register(scss);
+refractor.register(sass);
 refractor.register(bash);
-refractor.register(jsExtras);
-refractor.register(cssExtras);
 
 const withBundleAnalyzer = nextBundleAnalyzer({
     enabled: process.env.ANALYZE === "true",
@@ -29,47 +32,64 @@ const withMDX = nextMdx({
         providerImportSource: "@mdx-js/react",
         remarkPlugins: [remarkGfm, remarkToc],
         rehypePlugins: [
-            rehypePrism,
             rehypeSlug,
 
             /*
-        I did not find a clear way to highlight inline code blocks as jsx.
+        I did not find a clear way to highlight inline code blocks with specified language.
         RemarkPrism transformInlineCode works like unconfigurable shit, rehypePrism does not have this option.
       */
             [
                 rehypeRewrite,
                 {
-                    rewrite: node => {
+                    rewrite: (node, index, parent) => {
                         if (
                             node.tagName === "code" &&
                             node.children.every(child => child.type === "text")
                         ) {
+                            let combinedText = node.children
+                                .reduce((acc, v) => acc + v.value, "")
+                                .trim();
+
                             let lang;
 
-                            const newClassName =
-                                node.properties.className || [];
+                            const newClassName = (node.properties.className ||=
+                                []);
+
                             const langMatch = newClassName
                                 .find(c => c.startsWith("language-"))
                                 ?.match(/language-(.+)/);
 
-                            if (!langMatch) {
-                                newClassName.push("language-jsx");
-                                lang = "jsx";
-                            } else {
+                            if (langMatch) {
                                 lang = langMatch[1];
+                            } else {
+                                const dirtyLangMatch =
+                                    combinedText.match(/^~(.+)~/);
+
+                                if (dirtyLangMatch) {
+                                    lang = dirtyLangMatch[1];
+                                    combinedText = combinedText.slice(
+                                        dirtyLangMatch[0].length
+                                    );
+                                } else {
+                                    lang = "javascript";
+                                }
+
+                                newClassName.push(`language-${lang}`);
                             }
 
-                            const combinedText = node.children.reduce(
-                                (acc, v) => acc + v.value,
-                                ""
-                            );
                             const result = refractor.highlight(
                                 combinedText,
                                 lang
                             );
 
-                            node.properties.className = newClassName;
                             node.children = result.children;
+
+                            if (parent && parent.tagName === "pre") {
+                                // all syntax highlighters behave this way
+                                (parent.properties.className ||= []).push(
+                                    `language-${lang}`
+                                );
+                            }
                         }
                     }
                 }
@@ -87,7 +107,9 @@ const config = withBundleAnalyzer(
         pageExtensions: ["js", "jsx", "md", "mdx"],
         reactStrictMode: true,
         webpack(config) {
-            config.optimization.splitChunks.minSize = 1024;
+            if (config.optimization?.splitChunks) {
+                config.optimization.splitChunks.minSize = 1024;
+            }
             return config;
         },
         async redirects() {
