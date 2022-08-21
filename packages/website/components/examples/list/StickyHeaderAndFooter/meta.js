318b689088165ed2fc57232fb1e5b249e2dee695
alex <nowaalex@gmail.com>
2022-08-21 04:18:48 +0200

    corrected meta for sticky example

--- packages/website/components/examples/list/StickyHeaderAndFooter/meta.js
+++ packages/website/components/examples/list/StickyHeaderAndFooter/meta.js
@@ -1,2 +1,2 @@
 export const description =
-    "Virtual scroll can be used to render huge lists without performance problems. All dimensions are calculated automatically.";
+    "Header and footer can have sticky positioning. In order to use it, connect desired element to the model and add position: sticky style to it.";
