4382d2b334329264be4524e2d9822d1a2660714f
alex <nowaalex@gmail.com>
2023-10-05 06:00:24 +0200

    mobile fix

--- packages/website/components/layouts/Example/index.tsx
+++ packages/website/components/layouts/Example/index.tsx
@@ -5,7 +5,7 @@ const Example =
     ({ children }: { children: ReactNode }) => (
         <div className="flex flex-col h-full grow gap-4">
             {DescriptionComponent && <DescriptionComponent />}
-            <div className="grid grid-cols-1 xl:grid-cols-2 flex-1 gap-4 grow basis-0">
+            <div className="grid grid-cols-1 xl:grid-cols-2 flex-1 gap-4 grow xl:basis-0 xl:overflow-hidden xl:contain-strict">
                 <div className="overflow-hidden contain-strict not-prose xl:h-auto h-[40vh] grid">
                     {children}
                 </div>
