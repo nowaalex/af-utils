e308166956c8478d332650ceea695ed70be1e1a9
alex <nowaalex@gmail.com>
2023-10-22 10:33:02 +0200

    moved next image src away from public folder

--- packages/website/components/IndexExample/index.tsx
+++ packages/website/components/IndexExample/index.tsx
@@ -1,11 +1,10 @@
-"use client";
-
 import Image from "next/image";
+import imgSrc from "./messy.jpg";
 import List from "components/examples/react-examples/list/simple/code";
 
 const IndexExample = () => (
     <div className="grid md:grid-cols-2 gap-8 not-prose max-w-screen-xl">
-        <Image width={768} height={592} alt="messy" src="/messy.jpg" />
+        <Image width={768} height={592} alt="messy" src={imgSrc} />
         <div className="overflow-hidden contain-strict grid md:h-full h-64">
             <List />
         </div>
