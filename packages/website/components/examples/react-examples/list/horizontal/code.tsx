d06f08cdd95e8201855394c6a6ae701185c9e245
alex <nowaalex@gmail.com>
2023-10-25 01:17:34 +0200

    improved horizontal list example colors

--- packages/website/components/examples/react-examples/list/horizontal/code.tsx
+++ packages/website/components/examples/react-examples/list/horizontal/code.tsx
@@ -6,14 +6,9 @@ import { useVirtual, List, ListItemProps } from "@af-utils/virtual-react";
 const Item = memo<ListItemProps>(({ i, model }) => (
     <div
         ref={el => model.el(i, el)}
-        className={`
-        p-4
-        leading-[5em]
-        whitespace-nowrap
-        ${i % 2 ? "bg-orange-400" : "bg-orange-700"}
-    `}
+        className={"p-8" + (i % 2 ? "" : " bg-orange-100")}
     >
-        col {i}
+        col&nbsp;{i}
     </div>
 ));
 
@@ -24,7 +19,11 @@ const HorizontalList = () => {
         horizontal: true
     });
 
-    return <List model={cols}>{Item}</List>;
+    return (
+        <List model={cols} className="border border-slate-400">
+            {Item}
+        </List>
+    );
 };
 
 export default HorizontalList;
