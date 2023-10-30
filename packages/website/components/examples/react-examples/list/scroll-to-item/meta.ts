2078090e5cbff7f3e393cb2669d9aa5106e3f195
alex <nowaalex@gmail.com>
2023-10-30 13:33:57 +0100

    added scroll-to-index description & meta, upgraded terser

---
+++ packages/website/components/examples/react-examples/list/scroll-to-item/meta.ts
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+const metadata = {
+    description:
+        "VirtualScroller.scrollToIndex method \
+works with dynamic item sizes and performs scrolling until desired index is reached. Smooth scrolling is supported."
+} satisfies Metadata;
+
+export default metadata;
