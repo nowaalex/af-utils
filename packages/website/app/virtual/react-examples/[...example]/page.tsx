9adbe5c9249a3f3b81539188950db0482554fd58
alex <nowaalex@gmail.com>
2024-02-12 03:41:09 +0100

    reduced components quantity

--- packages/website/app/virtual/react-examples/[...example]/page.tsx
+++ packages/website/app/virtual/react-examples/[...example]/page.tsx
@@ -1,6 +1,6 @@
-import ExampleLayout from "components/Example";
 import { ShikiTransformer, codeToHtml } from "shiki";
 import theme from "shiki/dist/themes/github-light.mjs";
+import ExampleHeader from "components/ExampleHeader";
 import type { Metadata } from "next";
 
 type Params = { params: { example: string[] } };
@@ -92,13 +92,16 @@ const Page = async ({ params }: Params) => {
     );
 
     return (
-        <ExampleLayout
-            C={{
-                Code,
-                src,
-                Description
-            }}
-        />
+        <div className="flex flex-col h-full">
+            <div>
+                <ExampleHeader />
+                {Description && <Description />}
+            </div>
+            <div className="not-prose grid grid-cols-1 xl:grid-cols-2 flex-1 gap-4 grow xl:basis-0 xl:overflow-hidden xl:contain-strict">
+                <iframe className="xl:h-full h-[40vh] w-full" src={src} />
+                <Code className="overflow-x-auto xl:overflow-y-auto p-4 text-sm" />
+            </div>
+        </div>
     );
 };
 
