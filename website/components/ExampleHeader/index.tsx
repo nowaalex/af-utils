bdb00fee484f78c72de19b4925d2d57eeeedf86c
alex <nowaalex@gmail.com>
2024-02-20 12:47:14 +0100

    corrected example links

--- website/components/ExampleHeader/index.tsx
+++ website/components/ExampleHeader/index.tsx
@@ -20,24 +20,34 @@ const A = (props: ComponentPropsWithoutRef<"a">) => (
 
 const getHeader = (segments: string[]) => {
     switch (segments.length) {
-        case 3:
-            return `${segments[0]}: ${segments[2]} (${segments[1]})`;
-        case 2:
-            return `${segments[0]}: ${segments[1]}`;
+        case 5:
+            return `${segments[2]}: ${segments[4]} (${segments[3]})`;
+        case 4:
+            return `${segments[2]}: ${segments[3]}`;
         default:
             return "Example";
     }
 };
+
+const getGitPiece = (segments: string[]) => {
+    if (segments.length > 2) {
+        const pathPiece = segments.slice(3).join("/");
+
+        switch (segments[1]) {
+            case "virtual":
+                return `nowaalex/af-utils/tree/master/examples/src/virtual/react/${pathPiece}`;
+            case "scrollend-polyfill":
+                return `nowaalex/af-utils/tree/master/examples/src/scrollend-polyfill/${pathPiece}`;
+        }
+    }
+
+    throw new Error("Wrong segments");
+};
+
 const ExampleHeader = () => {
-    const segments = usePathname()
-        .replace("examples", "example")
-        .split("/")
-        .slice(2);
-
+    const segments = usePathname().replace("examples", "example").split("/");
     const startCasedSegments = segments.map(startCase);
-
-    const pathPiece = segments.slice(1).join("/");
-    const gitPiece = `nowaalex/af-utils/tree/master/examples/src/virtual/react/${pathPiece}`;
+    const gitPiece = getGitPiece(segments);
 
     return (
         <div className="flex flex-wrap gap-x-10 gap-y-4 items-center mb-6">
