4106cda38487cd72407740e447253ddd89c69efd
alex <nowaalex@gmail.com>
2024-03-09 03:19:46 +0100

    added glob sorting

--- examples/vite.config.js
+++ examples/vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import glob from "fast-glob";
 
-const htmls = glob.sync(["./src/**/index.html", "!**/lib/**index.html"]);
+const htmls = glob
+    .sync(["./src/**/index.html", "!**/lib/**index.html"])
+    .sort((a, b) => a.localeCompare(b));
 
 export default defineConfig({
     plugins: [react()],
