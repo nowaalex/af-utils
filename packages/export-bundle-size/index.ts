a628da708e5726998564dcd1ef7b874aa07f7067
alex <nowaalex@gmail.com>
2024-04-11 23:11:01 +0200

    upgraded deps & changed bundlesizes format

--- packages/export-bundle-size/index.ts
+++ packages/export-bundle-size/index.ts
@@ -3,7 +3,8 @@ import { existsSync } from "node:fs";
 import { parse, join } from "node:path";
 import { gzip as gzipCallback, brotliCompress } from "node:zlib";
 import { promisify } from "node:util";
-import { MinifyOptions, minify } from "terser";
+import { minify } from "terser";
+import type { MinifyOptions } from "terser";
 
 const tStart = performance.now();
 
@@ -18,71 +19,74 @@ const TERSER_OPTS = {
         comments: false,
         preserve_annotations: false
     }
-} satisfies MinifyOptions;
+} as const satisfies MinifyOptions;
+
+const EMPTY_FILE_SIZES = {
+    raw: 0,
+    min: 0,
+    minGz: 0,
+    minBrotli: 0
+} as const;
 
 const gzip = promisify(gzipCallback);
 const brotli = promisify(brotliCompress);
 
 async function write(
     fileName: string,
-    fileSizes: Awaited<ReturnType<typeof getFileSizes>>,
-    dts: boolean
+    fileSizes: Awaited<ReturnType<typeof getFileSizes>>
 ) {
-    const str = Object.entries(fileSizes)
-        .map(
-            ([name, size]) =>
-                `export ${dts ? "declare " : ""}const ${name} = ${size};`
+    return Promise.all(
+        [true, false].map(dts =>
+            writeFile(
+                dts ? fileName.replace(/.js$/, ".d.ts") : fileName,
+                Object.entries(fileSizes)
+                    .map(
+                        ([name, size]) =>
+                            `export ${dts ? "declare " : ""}const ${name} = ${size};`
+                    )
+                    .join("\n")
+            )
         )
-        .join("\n");
-
-    await writeFile(dts ? fileName.replace(/.js$/, ".d.ts") : fileName, str);
+    );
 }
 
 async function getFileSizes(code: string) {
-    const { code: minifiedCode } = await minify(code, TERSER_OPTS);
+    if (code) {
+        const { code: minifiedCode } = await minify(code, TERSER_OPTS);
 
-    if (minifiedCode) {
-        const [minifiedGzip, minifiedBrotli] = await Promise.all([
-            gzip(minifiedCode),
-            brotli(minifiedCode)
-        ]);
+        if (minifiedCode) {
+            const [minifiedGzip, minifiedBrotli] = await Promise.all([
+                gzip(minifiedCode),
+                brotli(minifiedCode)
+            ]);
 
-        return {
-            raw: code.length,
-            min: minifiedCode.length,
-            minGz: minifiedGzip.length,
-            minBrotli: minifiedBrotli.length
-        } as const;
+            return {
+                raw: code.length,
+                min: minifiedCode.length,
+                minGz: minifiedGzip.length,
+                minBrotli: minifiedBrotli.length
+            } as const;
+        }
     }
-
-    return {
-        raw: 0,
-        min: 0,
-        minGz: 0,
-        minBrotli: 0
-    } as const;
+    return EMPTY_FILE_SIZES;
 }
 
-for (let i = 2; i < process.argv.length; i++) {
-    const filePath = process.argv[i];
-    const parsedPath = parse(filePath);
-    const newFileName = join(
-        parsedPath.dir + `/bundlesize.${parsedPath.name}.js`
-    );
-    const fileContent = existsSync(filePath)
-        ? await readFile(filePath, { encoding: "utf-8" })
-        : "";
-
+const fileNameToRealEntry = async (fileName: string) => {
+    const fileContent = await readFile(fileName, { encoding: "utf-8" });
     const fileSizes = await getFileSizes(fileContent);
+    return [fileName, fileSizes];
+};
 
-    if (!existsSync(parsedPath.dir)) {
-        await mkdir(parsedPath.dir, { recursive: true });
-    }
-    await Promise.all([
-        write(newFileName, fileSizes, false),
-        write(newFileName, fileSizes, true)
-    ]);
-}
+const fileNameToEmptyEntry = (fileName: string) => [fileName, EMPTY_FILE_SIZES];
+
+const fileSizesEntries = await Promise.all(
+    process.argv.slice(2).map(fileNameToRealEntry)
+);
+
+await writeFile(
+    "lib/bundlesizes.js",
+    `export default ${JSON.stringify(Object.fromEntries(fileSizesEntries), null, "\t")}`
+);
 
 console.log(
     `Bundle sizes created in ${Math.round((performance.now() - tStart) * 100) / 100}ms`
