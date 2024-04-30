69cc4fcc873ed233bc0e633582b7ff69a81dc309
alex <nowaalex@gmail.com>
2024-04-30 02:22:24 +0200

    fixed examples tsconfigs/viteconfigs

--- examples/src/virtual/react/hook/custom-render/vite.config.ts
+++ examples/src/virtual/react/hook/custom-render/vite.config.ts
@@ -2,9 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    base: "",
-    build: {
-        outDir: "./dist"
-    }
+    plugins: [react()]
 });
